c3ac91fc56b1a0e7ee3df583751cc15bc59f1c70
Pradeep <pradeep.kumar@childrens.com>
2019-05-01 09:54:12 -0500

    first tests and absolute path imports

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+it('shows a comment box', () => {
+  const wrapped = shallow(<App />)
+
+  expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />)
+
+  expect(wrapped.find(CommentList).length).toEqual(1)
+})
